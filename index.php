09263649c1554683bbf2cfa7f6d30751c9e3d1f7
khalid567 <khalid05061997@gmail.com>
2020-05-30 17:50:02 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Coba git remote </title>
+</head>
+
+<body>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus nostrum perspiciatis ut omnis nemo soluta et eius quo ipsum, molestiae optio vitae repellendus debitis veritatis tenetur, numquam in laudantium iusto.</p>
+</body>
+
+</html>
