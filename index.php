204078ca834669700494686a6f2181a9eca19a57
khalid567 <khalid05061997@gmail.com>
2020-05-30 18:24:02 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
 </head>
 
 <body>
+    <h1>Hello world</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus nostrum perspiciatis ut omnis nemo soluta et eius quo ipsum, molestiae optio vitae repellendus debitis veritatis tenetur, numquam in laudantium iusto.</p>
+    <ul>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
